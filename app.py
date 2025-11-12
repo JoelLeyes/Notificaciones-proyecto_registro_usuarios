1e97cc60d00b7d1083af0fcc41cce5ea5f35d89b
ignaciogasco-prog <ignacio.gasco@estudiantes.utec.edu.uy>
2025-11-12 16:33:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ class UserObj:
 
 app = Flask(__name__)
 
+@app.route('/health')
+def health():
+    return jsonify({"status": "ok"}), 200
+
 
 @app.route('/notify', methods=['POST'])
 def notify():
@@ -27,3 +31,4 @@ def notify():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
+
